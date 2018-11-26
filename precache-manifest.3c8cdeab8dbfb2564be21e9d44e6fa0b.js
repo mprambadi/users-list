95ae2e034f006035fc72ad4087799abcc306ac95
Maul <mprambadi@gmail.com>
2018-11-26 11:57:59 +0700

    Updates

---
+++ precache-manifest.3c8cdeab8dbfb2564be21e9d44e6fa0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a62002af5d334ab8bb66",
+    "url": "/users-list/static/js/runtime~main.a62002af.js"
+  },
+  {
+    "revision": "5789be002f68196a8e9c",
+    "url": "/users-list/static/js/main.5789be00.chunk.js"
+  },
+  {
+    "revision": "d0ad83403d04aafa6ba6",
+    "url": "/users-list/static/js/1.d0ad8340.chunk.js"
+  },
+  {
+    "revision": "d0ad83403d04aafa6ba6",
+    "url": "/users-list/static/css/1.26a6814d.chunk.css"
+  },
+  {
+    "revision": "5746a804699192802babd27988691d05",
+    "url": "/users-list/index.html"
+  }
+];
